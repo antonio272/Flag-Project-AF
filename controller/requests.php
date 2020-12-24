a9431bf358872d4d406b63006d599202849cc1b9
Antonio Frade <you@example.com>
2020-12-24 17:08:05 +0000

    Actualizado controller de requests de AJAX para dropdowns dinamicas, incluindo implementação de função data/hora para marcação de consultas, com limitação de dias e horas disponíveis, dependente da especialidade e médico escolhido.

--- controller/requests.php
+++ controller/requests.php
@@ -3,7 +3,38 @@ header("Content-Type: application/json");
 
 if( isset($_GET["request"]) ) {
 
-    if( $_GET["request"] ==="selectDoctors" &&
+    if( $_GET["request"] ==="selectDatehour" &&
+        isset($_GET['doctor_id']) && 
+        isset($_GET['specialty_id'])) {
+
+
+      $today = strtotime(date("Y-m-d"));
+
+      for($i = $today; $i < $today + (30*24*60*60); $i = $i + (24 * 60 * 60) ) {
+
+        $date = date("Y-m-d", $i);
+
+        for($j = 9; $j <= 15; $j++) {
+
+            if($j<10){
+            $possible_dates[] = [
+                'id' => $date . " " . "0".$j.":00:00",
+                'value' => $date . " " . "0".$j.":00:00"
+            ];
+            }else{
+            $possible_dates[] = [
+                'id' => $date . " " . $j.":00:00",
+                'value' => $date . " " . $j.":00:00"
+            ];
+            }
+        }
+      }
+
+      echo json_encode($possible_dates);
+
+
+
+    }else if( $_GET["request"] ==="selectDoctors" &&
         !empty($_GET['specialty_id']) ) {
 
         require("../model/doctors.php");
